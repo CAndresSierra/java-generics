e6cd5236548cb22cabe7d2e695434983878b3d9f
CAndresSierra <camilofullstack@gmail.com>
2025-10-07 20:43:19 -0500

    feat(core): Implement generic DataStore
    Implements a generic DataStore class using generics
    for storing and retrieving data with a key. Adds
    methods for adding, removing, finding, and listing
    data.

--- src/main/java/com/devtalles/proyect/datastore/DataStore.java
+++ src/main/java/com/devtalles/proyect/datastore/DataStore.java
@@ -1,4 +1,45 @@
 package com.devtalles.proyect.datastore;
 
-public class DataStore {
+import java.util.*;
+
+public class DataStore<T, K> {
+    private List<T> usersNames = new LinkedList<>();
+    private Map<K, T> users = new HashMap<>();
+
+    public void add(K key, T item)throws NoSuchElementException {
+        if(!users.containsKey(key)){
+            users.put(key, item);
+            usersNames.add(item);
+            System.out.println("User has been added successfully");
+
+        } else {
+            throw new NoSuchElementException("The user with ID: " + key + " already exists.");
+        }
+    }
+
+    public void remove(K key, T item)throws NoSuchElementException {
+        if(users.containsKey(key)){
+            users.remove(key);
+            usersNames.remove(item);
+            System.out.println("The user has been deleted succesfully");
+        } else {
+            throw new NoSuchElementException("User with ID: " + key + " don't exists.");
+        }
+    }
+
+    public T find(K key){
+        if(users.containsKey(key)){
+            return users.get(key);
+        }
+        return null;
+    }
+
+    public List<T> getAll()throws NoSuchElementException{
+        if(!usersNames.isEmpty()){
+            return usersNames;
+        } else {
+            throw new NoSuchElementException("There arent data.");
+        }
+    }
+
 }
